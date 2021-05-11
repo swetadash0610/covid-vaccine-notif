de979c7c3016d7b7bb539a7f7ed108bef1cab09b
swetadash0610 <swetadash0610@gmail.com>
2021-05-11 14:15:11 +0530

    bugs fixed

--- main.py
+++ main.py
@@ -9,27 +9,34 @@ load_dotenv()
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
-from_ =os.environ['FROM']
-to =os.environ['TO']
+from_ = os.environ['FROM']
+to = os.environ['TO']
 
 district_id = os.environ['DIST']
-min_age_limit = 18
+min_age_limit = os.environ['AGE']
 pin_code = os.environ['PIN']
 cowin = CoWinAPI()
 available_centers = cowin.get_availability_by_district(
     district_id, min_age_limit)
 available_centers = available_centers['centers']
 
+# If successfully connected , you will receive this message on first run
+success = "You are successsfully connected to vaccine slot notifier. You will receive a message if slots are available in district ID "+str(district_id)+" and has a minimum age limit of "+str(min_age_limit)+" years"
+message = client.messages.create(
+    body=success,
+    from_=from_,
+    to=to
+)
+
 while True:
     for value in available_centers:
         for session in value['sessions']:
-            body = str(value['name'])+"-"+str(session['date']) + \
-                "- "+str(session['available_capacity'])
-            if session['min_age_limit'] == 18 and session['available_capacity'] != 0:
+            body = str(value['name'])+" Date: "+str(session['date']) + \
+                " Slots: "+str(session['available_capacity'])
+            if session['min_age_limit'] == int(min_age_limit) and session['available_capacity'] != 0:
                 message = client.messages.create(
                     body=body,
                     from_=from_,
                     to=to
                 )
-                print(message.sid)
-    time.sleep(30.0 - ((time.time() - starttime) % 60.0))
+    time.sleep(30)
